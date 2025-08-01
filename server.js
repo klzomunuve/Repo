a4b2351287106c9d147c23a9bcc4ff1fac088009
klzomunuve <klzoalfredoh@gmail.com>
2025-08-01 19:15:56 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import authRoutes from './routes/auth.js';
 
+dotenv.config();
 const app = express();
-const port = process.env.PORT || 3000;
+app.use(cors());
+app.use(express.json());
 
-// Simple route
-app.get('/', (req, res) => {
-  res.send('✅ Server is working with ES Modules!');
-});
+app.use("/api/auth", authRoutes);
 
-// Start server
-app.listen(port, () => {
-  console.log(`🚀 Server is running on port ${port}`);
-});
+const PORT = process.env.PORT || 5000;
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => app.listen(PORT, () => console.log("Server is working with ES Modules!")))
+  .catch((err) => console.error(err));
