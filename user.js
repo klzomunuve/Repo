d4611ed7f0189ef35eb7e6d7307f1674dd3e5876
klzomunuve <klzoalfredoh@gmail.com>
2025-08-01 19:16:53 +0300

    Update user.js

--- user.js
+++ user.js
@@ -1,15 +1,8 @@
-const mongoose = require("mongoose");
+import mongoose from 'mongoose';
 
-const userSchema = new mongoose.Schema({
-  email: {
-    type: String,
-    unique: true,
-    required: true
-  },
-  password: {
-    type: String,
-    required: true
-  }
+const UserSchema = new mongoose.Schema({
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true }
 });
 
-module.exports = mongoose.model("User", userSchema);
+export default mongoose.model("User", UserSchema);
